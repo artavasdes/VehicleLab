371fbd9d082e59aca9f5e0238caed3be6ba116a1
aaron <aarwil24@bergen.org>
2022-03-14 09:33:49 -0400

    kinda teslaModelZ

--- vehicle/TeslaModelZ.java
+++ vehicle/TeslaModelZ.java
@@ -1,5 +1,25 @@
 package vehicle;
 
-public class TeslaModelZ {
+public class TeslaModelZ extends ElectricCar{
+    public TeslaModelZ(double startingMileage, int modelNum){
+        this.mileage = startingMileage;
+        this.modelNum = modelNum;
+    }
     
+    public TeslaModelZ(int modelNum){
+        this.mileage = 0;
+        this.modelNum = modelNum;
+    }
+
+    public int getModelNum(){
+        return modelNum;
+    }
+
+    public String getModel(){
+        return model + modelNum; 
+    }
+
+    public String toString(){
+
+    }
 }
