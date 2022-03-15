77a9602698a638aba6efc654893f71b3f963970e
artavasdes <56280139+artavasdes@users.noreply.github.com>
2022-03-15 09:29:40 -0400

    started work on ford frivolous

--- vehicle/FordFrivolous.java
+++ vehicle/FordFrivolous.java
@@ -1,5 +1,12 @@
 package vehicle;
 
-public class FordFrivolous {
+public class FordFrivolous extends GasPoweredCar{
+    public FordFrivolous(double startingMileage){
+       super("Ford", "Frivolous", startingMileage, 23.6, 20.0);
+    }
+
+    public FordFrivolous(){
+        super("Ford", "Frivolous", 23.6, 20.0);
+    }
     
 }
