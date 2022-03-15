9b33a39dab64bc5352ad4b9db5269903e20616d0
artavasdes <56280139+artavasdes@users.noreply.github.com>
2022-03-15 09:44:11 -0400

    finished ford frivolous

--- vehicle/FordFrivolous.java
+++ vehicle/FordFrivolous.java
@@ -1,12 +1,39 @@
 package vehicle;
 
-public class FordFrivolous extends GasPoweredCar{
+public class FordFrivolous extends GasPoweredCar implements SelfDriving, Flying{
     public FordFrivolous(double startingMileage){
        super("Ford", "Frivolous", startingMileage, 23.6, 20.0);
     }
 
     public FordFrivolous(){
         super("Ford", "Frivolous", 23.6, 20.0);
+    }
+
+    public void driveAutonomously(double miles){
+        if(miles < 0){
+            throw new IllegalArgumentException("Miles cannot be negative!");
+        }
+        super.drive(miles);
+        super.decreaseFuelLevel(miles);
+    }
+
+    public boolean canFly(double miles){
+        if(miles < 0){
+            throw new IllegalArgumentException("Miles cannot be negative!");
+        }
+        return true;
+    }
+
+    public void fly(double miles){
+        if(miles < 0){
+            throw new IllegalArgumentException("Miles cannot be negative!");
+        }
+        if(getRemainingRange()<=0){
+            throw new IllegalArgumentException("Miles exceeds remaining range!");
+        }
+        super.drive(miles);
+        super.decreaseFuelLevel(miles);
+        super.decreaseFuelLevel(miles);
     }
     
 }
