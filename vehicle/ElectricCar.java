34983f69779f4417ddbdab8dfc4145ae18e8f347
aaron <aarwil24@bergen.org>
2022-03-15 09:27:08 -0400

    electricCar

--- vehicle/ElectricCar.java
+++ vehicle/ElectricCar.java
@@ -1,5 +1,45 @@
 package vehicle;
 
-public abstract class ElectricCar {
+public abstract class ElectricCar extends Car {
+    private double milesOnMaxCharge;
+    private double remaingingCharge;
     
+    public ElectricCar(String make, String model, double startingMileage, double milesOnMaxCharge){
+        super(make, model, mileage);
+        if(milesOnMaxCharge < 0){
+            throw new IllegalArgumentException();
+        }
+        
+        this.remaingingCharge = milesOnMaxCharge;
+        this.milesOnMaxCharge = milesOnMaxCharge;
+        
+    }
+
+    public ElectricCar(String make, String model, double milesOnMaxCharge){
+        this(make, model, milesOnMaxCharge);
+        this.mileage = 0;
+    }
+
+    public void drive(double miles){
+        if(miles < 0 || miles > getRemainingRange()){
+            throw new IllegalArgumentException();
+        }
+        mileage += miles;
+    }
+
+    public double getRemainingRange(){
+        return remaingingCharge;
+    }
+
+    public double getMaxRange(){
+        return milesOnMaxCharge;
+    }
+
+    public void recharge(){
+        remaingingCharge = milesOnMaxCharge;
+    }
+
+    protected void decreaseCharge(double miles){
+        remaingingCharge -= miles;
+    }
 }
