0a645ca5d9727f996ee0f177778036d15e11325b
aaron <aarwil24@bergen.org>
2022-03-19 21:12:28 -0400

    Attempted to finish TestFordFrivolous

--- tests/TestFordFrivolous_Driving.java
+++ tests/TestFordFrivolous_Driving.java
@@ -40,19 +40,33 @@ public class TestFordFrivolous_Driving extends BCATestScenario {
         assertEquals(f1.toString(), "Ford Frivolous (118 mi)", "toString does not match");
         
         //Stopped at row 26 on excel file
+        assertThrows(IllegalArgumentException.class, () -> {f1.roadTrip(50, 50, 50, -100)}, "Mileage in any position of the list cannot be negative");
+        
+        f1.roadTrip(100, 100, 100, 100);
+        assertEquals(f1.getMileage(), 590, .1, "Mileage should be 590");
+        
+        assertEquals(f1.getRemainingRange(), 0, .1, "Remaining range should be 0");
 
-        c1.drive(200);
-        assertEquals(c1.getMileage(), 230, .1, "Mileage should be 230 after second drive.");
+        f1.refillTank(10);
+        assertEquals(f1.getFuelLevel(), 10, .1, "Fuel level should now be 10");
 
-        assertEquals(c1.getRemainingRange(), c1.getFuelCapacity() * c1.getMPG() - 230, .1, "Remaining range of car not correct after driving twice.");
+        f1.roadTrip(100, 100, 36);
 
-        assertFalse(c1.canDrive(252), "Driving 252 should fail.");
-        assertTrue(c1.canDrive(251), "Driving 251 should succeed.");
+        assertEquals(f1.getRemainingRange(), 0, .1, "Remaining range should be 0");
 
-        c1.drive(251);
-        assertEquals(c1.getMileage(), 481, .1, "Mileage should be 481 after third drive.");
 
-        assertThrows(IllegalArgumentException.class, () -> {c1.drive(5);}, "Driving beyond empty should fail.");
+        // c1.drive(200);
+        // assertEquals(c1.getMileage(), 230, .1, "Mileage should be 230 after second drive.");
+
+        // assertEquals(c1.getRemainingRange(), c1.getFuelCapacity() * c1.getMPG() - 230, .1, "Remaining range of car not correct after driving twice.");
+
+        // assertFalse(c1.canDrive(252), "Driving 252 should fail.");
+        // assertTrue(c1.canDrive(251), "Driving 251 should succeed.");
+
+        // c1.drive(251);
+        // assertEquals(c1.getMileage(), 481, .1, "Mileage should be 481 after third drive.");
+
+        // assertThrows(IllegalArgumentException.class, () -> {c1.drive(5);}, "Driving beyond empty should fail.");
 
 
         return getFailedCount();
